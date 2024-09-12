e37cef0859fa9837949857eae669efaac6796c16
Made Mas Adi Winata <m45adiwinata@gmail.com>
2024-09-12 09:34:11 +0700

    change command namespace

--- src/Commands/DtoMakeCommand.php
+++ src/Commands/DtoMakeCommand.php
@@ -13,7 +13,7 @@
 
 declare(strict_types=1);
 
-namespace App\Console\Commands;
+namespace Spotlibs\PhpLib\Commands;
 
 use Illuminate\Console\GeneratorCommand;
 use Symfony\Component\Console\Input\InputOption;
